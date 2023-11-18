f7a5c1350cadd5c844fdac432ac8af8707a4209c
Azhar Ali <101246889+Azhar-Ali-BSTM119-34@users.noreply.github.com>
2023-11-19 01:00:19 +0500

    5th

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
 import Authentication from "./Components/auth";
+import { db } from "./confi/firebase";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    // Assuming moviesCollectionRef is a reference to a Firestore collection
+    const moviesCollectionRef = collection(db, "movies");
+
+    const getMoviesList = async () => {
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        movieList(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getMoviesList();
+  }, []);
+
   return (
     <>
       <Authentication />
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h2>{movie.title}</h2>
+            <p> Date: {movie.releaseData}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/confi/firebase.jsx
+++ src/confi/firebase.jsx
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
