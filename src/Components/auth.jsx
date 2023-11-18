e55ead1223f657fc4ba1c80876c8ed996a4568a1
Azhar Ali <101246889+Azhar-Ali-BSTM119-34@users.noreply.github.com>
2023-11-18 21:18:54 +0500

    3rd commit

--- src/Components/auth.jsx
+++ src/Components/auth.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { auth } from "../confi/firebase";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Authentication() {
-  return <div>Authentication</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="h-full w-full bg-black flex justify-center items-center">
+      <div className="bg-white p-8 rounded shadow">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block mb-2">
+            email
+          </label>
+          <input
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full border border-gray-300 rounded px-3 py-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block mb-2">
+            Password
+          </label>
+          <input
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full border border-gray-300 rounded px-3 py-2"
+          />
+        </div>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={signIn}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Authentication;
